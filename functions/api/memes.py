5c7069900d5c43714f0dfede083d92aaf50126ab
Paul-Tru <99089100+Paul-Tru@users.noreply.github.com>
2023-11-27 23:12:21 +0100

    Made memes.py run 5 times, fixed title error

--- functions/api/memes.py
+++ functions/api/memes.py
@@ -3,9 +3,11 @@ import functions.tools.tools as t
 
 
 def memes():
-    response = requests.get("https://meme-api.com/gimme")
-    data = response.json()
-    pic = data.get("url")
-    spoiler = data.get("spoiler")
-    if spoiler is not True:
-        t.save_pics(pic, "memes", pic)
+    for x in range(5):
+        response = requests.get("https://meme-api.com/gimme")
+        data = response.json()
+        pic = data.get("url")
+        title = pic.replace("https://i.redd.it/", "")
+        spoiler = data.get("spoiler")
+        if spoiler is not True:
+            t.save_pics(pic, "memes", title)
