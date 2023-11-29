23db3869da62d224fbaed613047bad06ecf4f2f0
Paul-Tru <99089100+Paul-Tru@users.noreply.github.com>
2023-11-29 19:19:28 +0100

    Adding pdf tools (bugs)

---
+++ functions/tools/pdf.py
@@ -0,0 +1,46 @@
+from PyPDF2 import PdfReader, PdfWriter, PdfMerger
+
+
+def get_meta(path):
+    with open(path, 'rb') as f:
+        pdf = PdfReader(f)
+        info = pdf.getDocumentInfo()
+        nop = pdf.getNumPages()
+    print(info)
+    print(nop)
+
+
+def extract_text(path):
+    with open(path, 'rb') as f:
+        pdf = PdfReader(f)
+        page_one = pdf.pages[0]
+        text = page_one.extract_text()
+        return text
+
+
+def split_pdf(path):
+    with open(path, 'rb') as f:
+        pdf = PdfReader(f)
+        for page in range(pdf.numPages()):
+            writer = PdfWriter()
+            writer.addPage(pdf.pages(page))
+            with open(f"{page}.pdf", "wb") as f_out:
+                writer.write(f_out)
+
+
+def merge_pdf(input_paths, output_path):
+    writer = PdfWriter()
+    for i in input_paths:
+        pdf_reader = PdfReader(i)
+        for page in range(pdf_reader.NumPages()):
+            writer.addPage(pdf_reader.pages(page))
+    with open(output_path, "wb") as f_out:
+        writer.write(f_out)
+
+
+def lazy_merge(input_paths, output_path):
+    pdf_merger = PdfMerger()
+    for i in input_paths:
+        pdf_merger.append(i)
+    with open(output_path, "wb") as f_out:
+        pdf_merger.write(f_out)
