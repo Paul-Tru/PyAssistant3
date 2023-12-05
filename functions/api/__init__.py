22a115b98cb5b9d664ed1e1a4b45cd48fb5a9ccf
Paul-Tru <99089100+Paul-Tru@users.noreply.github.com>
2023-12-05 23:59:13 +0100

    Using __init__ files

---
+++ functions/api/__init__.py
@@ -0,0 +1,4 @@
+from .memes import memes
+from .tagesschau import tagesschau
+from .weather import weather
+from .wikipedia import wikipedia
---
+++ functions/gui/__init__.py
@@ -0,0 +1,3 @@
+from dashboard import dashboard
+from out import answer
+from rename import tools_rename
