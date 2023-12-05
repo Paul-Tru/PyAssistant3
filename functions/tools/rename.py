9e49ef109d77f96b96f51e559c900122c538d173
Paul-Tru <99089100+Paul-Tru@users.noreply.github.com>
2023-12-06 00:01:40 +0100

    Updating at rename

--- functions/tools/rename.py
+++ functions/tools/rename.py
@@ -4,19 +4,21 @@ import os
 def rename_files(dic_path):
     if " " in dic_path:
         dic_path = dic_path.replace(" ", "_")
+    try:
+        for counter, filename in enumerate(os.listdir(dic_path)):
+            if os.path.isfile(os.path.join(dic_path, filename)):
 
-    for counter, filename in enumerate(os.listdir(dic_path)):
-        if os.path.isfile(os.path.join(dic_path, filename)):
+                file_type = filename.split(".")[-1]
+                name = check_filename(dic_path)
+                new_name = f"{name}-{counter}{os.path.splitext(dic_path)[1]}.{file_type}"
 
-            file_type = filename.split(".")[-1]
-            name = check_filename(dic_path)
-            new_name = f"{name}-{counter}{os.path.splitext(dic_path)[1]}.{file_type}"
+                # Rename data
+                os.rename(os.path.join(dic_path, filename), os.path.join(dic_path, new_name))
 
-            # Rename data
-            os.rename(os.path.join(dic_path, filename), os.path.join(dic_path, new_name))
-
-        print(counter, filename, dic_path)
-    print(f"All files in '{dic_path}' were renamed.")
+            print(counter, filename, dic_path)
+        return f"All files in '{dic_path}' were renamed."
+    except Exception as e:
+        return f"Error {e} while trying to rename files in '{dic_path}'"
 
 
 def rename_dictionary(path, new_path):
